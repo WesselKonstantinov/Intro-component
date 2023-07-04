42d44059119e53f3f041461e01f94a89dff16ad8
Wessel Konstantinov <wes_kon1@hotmail.com>
2023-07-04 21:21:13 +0200

    Refactor implementation

--- script.js
+++ script.js
@@ -1,107 +1,85 @@
-class FormValidator {
-  constructor(form, fields) {
-    this.form = form;
-    this.fields = fields;
+const form = document.querySelector(".js-form");
+const fields = document.querySelectorAll(".js-input");
+
+const setStatus = (field, message = "") => {
+  const formErrorMessage = field.parentElement.querySelector(
+    ".js-form-error-message"
+  );
+
+  if (message) {
+    formErrorMessage.innerText = message;
+    field.setAttribute("aria-invalid", "true");
+  } else {
+    formErrorMessage.innerText = "";
+    field.removeAttribute("aria-invalid");
   }
+};
 
-  initialize() {
-    this.validateOnEntry();
-    this.validateOnSubmit();
-  }
+const validate = (field) => {
+  const isEmpty = field.value.trim() === "";
 
-  validateOnSubmit() {
-    let self = this;
+  if (field.type === "text") {
+    if (isEmpty) {
+      setStatus(
+        field,
+        `${field.previousElementSibling.innerText} cannot be empty`
+      );
+    } else {
+      setStatus(field);
+    }
+  } else if (field.type === "email") {
+    const emailRegex = /\S+@\S+\.\S+/;
 
-    this.form.addEventListener("submit", (e) => {
-      e.preventDefault();
-      self.fields.forEach((field) => {
-        const input = document.querySelector(`.js-${field}-field`);
-        self.validateFields(input);
-      });
-    });
-  }
+    if (isEmpty) {
+      setStatus(
+        field,
+        `${field.previousElementSibling.innerText} cannot be empty`
+      );
+    } else if (!emailRegex.test(field.value)) {
+      setStatus(field, "Looks like this is not an email");
+    } else {
+      setStatus(field);
+    }
+  } else if (field.type === "password") {
+    const passwordRegex =
+      /^(?!.*\s)(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]).{8,16}$/;
 
-  validateOnEntry() {
-    let self = this;
-    this.fields.forEach((field) => {
-      const input = document.querySelector(`.js-${field}-field`);
-      input.addEventListener("input", () => self.validateFields(input));
-    });
-  }
-
-  validateFields(field) {
-    const isEmpty = field.value.trim() === "";
-
-    if (field.type === "text") {
-      if (isEmpty) {
-        this.setStatus(
-          field,
-          `${field.previousElementSibling.innerText} cannot be empty`,
-          "error"
-        );
-      } else {
-        this.setStatus(field, null, "success");
-      }
-    } else if (field.type === "email") {
-      const emailRegex = /\S+@\S+\.\S+/;
-
-      if (isEmpty) {
-        this.setStatus(
-          field,
-          `${field.previousElementSibling.innerText} cannot be empty`,
-          "error"
-        );
-      } else if (!emailRegex.test(field.value)) {
-        this.setStatus(field, "Looks like this is not an email", "error");
-      } else {
-        this.setStatus(field, null, "success");
-      }
-    } else if (field.type === "password") {
-      const passwordRegex =
-        /^(?!.*\s)(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]).{8,16}$/;
-
-      if (field.value.length === 0) {
-        this.setStatus(
-          field,
-          `${field.previousElementSibling.innerText} cannot be empty`,
-          "error"
-        );
-      } else if (!passwordRegex.test(field.value)) {
-        this.setStatus(
-          field,
-          `The Password must not contain any whitespace.
+    if (field.value.length === 0) {
+      setStatus(
+        field,
+        `${field.previousElementSibling.innerText} cannot be empty`
+      );
+    } else if (!passwordRegex.test(field.value)) {
+      setStatus(
+        field,
+        `The Password must not contain any whitespace.
         The Password must contain at least one uppercase character.
         The Password must contain at least one lowercase character.
         The Password must contain at least one digit.
         The Password must have at least one special symbol.
-        The Password must be 8-16 characters long.`,
-          "error"
-        );
-      } else {
-        this.setStatus(field, null, "success");
-      }
+        The Password must be 8-16 characters long.`
+      );
+    } else {
+      setStatus(field);
     }
   }
+};
 
-  setStatus(field, message, status) {
-    const formErrorMessage = field.parentElement.querySelector(
-      ".js-form-error-message"
-    );
+fields.forEach((field) => {
+  field.addEventListener("input", (e) => validate(e.target));
+  field.addEventListener("blur", (e) => validate(e.target));
+});
 
-    if (status === "success") {
-      formErrorMessage.innerText = "";
-      field.removeAttribute("aria-invalid");
-    }
-
-    if (status === "error") {
-      formErrorMessage.innerText = message;
-      field.setAttribute("aria-invalid", "true");
-    }
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fields.forEach((field) => validate(field));
+  if (
+    Array.from(fields).some(
+      (field) => field.getAttribute("aria-invalid") === "true"
+    )
+  ) {
+    form.querySelector("[aria-invalid='true']").focus();
+  } else {
+    console.log("Successfully submitted");
   }
-}
-
-const form = document.querySelector(".js-form");
-const fields = ["first-name", "last-name", "email", "password"];
-
-const formValidator = new FormValidator(form, fields);
-formValidator.initialize();
+});
